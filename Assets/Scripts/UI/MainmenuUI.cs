4eada536b80fae87e145ef37d49a1cae615a928c
ajwad_imran <ajwadimran7@gmail.com>
2019-02-23 18:08:07 +0500

    Project + Slides

---
+++ Assets/Scripts/UI/MainmenuUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainmenuUI : MonoBehaviour
+{
+    public void Play() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+}

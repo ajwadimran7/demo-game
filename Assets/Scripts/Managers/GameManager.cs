4eef1137e4522aced5716b2dd3b2411f71dc1449
Ajwad <ajwad@quixel.se>
2019-02-24 11:33:24 +0500

    Level design

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance;
+
+    void Awake() {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            SceneManager.LoadScene(0);
+        }
+    }
+}

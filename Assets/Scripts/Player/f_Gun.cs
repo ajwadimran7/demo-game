89c5d03a266e8f29d0a82b0868ae2ee180c7665c
Ajwad <ajwad@quixel.se>
2019-02-24 12:24:43 +0500

    partial level setup.

---
+++ Assets/Scripts/Player/f_Gun.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class f_Gun : MonoBehaviour
+{
+
+    public Transform firePoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
